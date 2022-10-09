1495b637f186b9cd8ee905bc6f21e98b9f785e83
Vikram Auradkar <vikram.auradkar@gmail.com>
2022-10-09 08:10:02 -0700

    Fix: ctl when held.

--- keyboards/crkbd/keymaps/vauradkar/keymap.c
+++ keyboards/crkbd/keymaps/vauradkar/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    LCTL(KC_Z),    LALT(KC_X),    KC_C,    KC_V,    KC_B,                             KC_N,    KC_M, KC_COMM,  LALT(KC_DOT), LCTL(KC_SLSH),  KC_ESC,
+      KC_LSFT,    LCTL_T(KC_Z),    LALT_T(KC_X),    KC_C,    KC_V,    KC_B,                             KC_N,    KC_M, KC_COMM,  LALT_T(KC_DOT), LCTL_T(KC_SLSH),  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|                           |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,  KC_SPC,   MO(1),                               MO(2),  KC_ENT, KC_RALT
                                       //`--------------------------'                           `--------------------------'
